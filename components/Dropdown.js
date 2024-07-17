5a337058ab790551e2e05441cf6c1bdb05a7f665
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-07-17 17:57:08 +0300

    chore: Add Dropdown component
    This commit adds a new Dropdown component to the project. The Dropdown component is a reusable component that displays a dropdown menu with a list of options. It includes styling for position, background color, shadow, border, and maximum height. The component also supports scrolling and nested scrolling.

---
+++ components/Dropdown.js
@@ -0,0 +1,47 @@
+import { StyleSheet, ScrollView } from 'react-native';
+import COLORS from '../constants/COLORS';
+
+const defaultStyle = StyleSheet.create({
+  dropdown: {
+    position: 'absolute',
+    top: '100%',
+    width: '100%',
+    zIndex: 1,
+    backgroundColor: 'white',
+    borderRadius: 5,
+    padding: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    borderWidth: 1,
+    borderColor: COLORS.border_grey,
+    maxHeight: 200,
+    marginTop: 10,
+  },
+  conntentStyle: {
+    gap: 5,
+  },
+});
+
+export default function Dropdown({ ...props }) {
+  const { visible, children, onScroll } = props;
+
+  if (!visible) return null;
+
+  const style = props.style || {};
+
+  return (
+    <ScrollView
+      contentContainerStyle={defaultStyle.conntentStyle}
+      style={[defaultStyle.dropdown, style.dropdown]}
+      scrollEnabled={true}
+      nestedScrollEnabled={true}
+      onScroll={onScroll}
+    >
+      
+      {children}
+    </ScrollView>
+  );
+}
